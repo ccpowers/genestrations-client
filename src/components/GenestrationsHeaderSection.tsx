6bcbc722e21bfba571f2130a89c61323026482f9
ccpowers <katypm@gmail.com>
2023-09-03 23:12:05 -0400

    wip

--- src/components/GenestrationsHeaderSection.tsx
+++ src/components/GenestrationsHeaderSection.tsx
@@ -10,12 +10,14 @@ interface GenestrationsHeaderSectionProps  {
 const GenestrationsHeaderSection: React.FC<GenestrationsHeaderSectionProps> = ({gameStatus, start, reset}) => {
 
     return (
-        <div className="text-3xl font-bold underline flex row">
+        <div className="text-3xl text-white font-bold underline flex row">
            <h1>Genestrations</h1>
-           <button onClick={start} className="bg-violet-500 text-white active:bg-violet-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
+           <button onClick={start} disabled={gameStatus !== 'PENDING'} className="bg-violet-500 shawdow-md text-white active:bg-violet-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
             Start
             </button>
-           <Button onClick={reset}>Reset</Button>
+            <button onClick={reset} className="bg-violet-500 text-white active:bg-violet-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
+            Reset
+            </button>
         </div>
     );
 };
